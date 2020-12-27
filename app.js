94f92c5bd9197ba89cd3edb42e1366384babbc1f
tanwar1997 <sainishubham799@gmail.com>
2020-12-28 00:32:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useUnifiedTopology: true, useNewUrlParser: true });
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welocme to our todolist"
+});
+const item2 = new Item({
+  name: "Hit the + button to add new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item "
+});
+const defaultItems = [item1, item2, item3];
+
+
+app.get("/", function (req, res) {
+  Item.find({}, (err, fountItems) => {
+    if (fountItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+
+        }
+        else {
+          console.log("successfully saved default items in databse");
+        }
+      })
+      res.redirect("/")
+    }
+    else {
+      res.render("list", { listTitle: "today", newListItems: fountItems });
+    }
+  })
+});
+
+app.post("/", function (req, res) {
+
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+  item.save()
+  res.redirect("/")
+});
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, (err, docs) => {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log("removed item: " + docs)
+      res.redirect("/")
+    }
+  })
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
